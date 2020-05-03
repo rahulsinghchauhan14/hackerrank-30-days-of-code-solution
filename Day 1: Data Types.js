64ee2e11999c3e5ff986790d93a5d6bf2bdd0703
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2020-05-03 15:22:39 +0530

    Day 1
    learn what is data type and how can we declare in js

---
+++ Day 1: Data Types.js
@@ -0,0 +1,24 @@
+function main() {
+    var i = 4
+    var d = 4.0
+    var s = "HackerRank "
+    // Declare second integer, double, and String variables.
+    var i2 = 3
+    var d2 = 2.8
+    var s2 = "is my favorite platform!"
+    // Read and save an integer, double, and String to your variables.
+    var is = i+i2
+    var ds = parseFloat(d)+parseFloat(d2)
+    var ss = s+''+ s2
+    // Print the sum of both integer variables on a new line.
+    console.log(is);
+   
+    // Print the sum of the double variables on a new line.
+     console.log(ds.toFixed(1));
+
+    // Concatenate and print the String variables on a new line
+    console.log(ss);
+
+    // The 's' variable above should be printed first.
+
+}
